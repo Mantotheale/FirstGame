76cce8b28f4521028496265302d64b13595d0138
mantotheale <alessiomantonico@gmail.com>
2026-06-01 01:25:30 +0900

    Added OpenGL functionality

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,33 @@ use glfw::{Action, Context, Key};
 fn main() {
     use glfw::fail_on_errors;
     let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+    glfw.window_hint(glfw::WindowHint::Resizable(false));
 
     let (mut window, events) = glfw
         .create_window(300, 300, "Hello this is window", glfw::WindowMode::Windowed)
         .expect("Failed to create GLFW window.");
+    let (screen_width, screen_height) = window.get_framebuffer_size();
 
     window.make_current();
+    gl::load_with(|symbol| {
+        window
+            .get_proc_address(symbol)
+            .map(|a| a as *const _)
+            .unwrap_or(std::ptr::null())
+    });
+
+    unsafe {
+        gl::Viewport(0, 0, screen_width, screen_height);
+        gl::ClearColor(0.4, 0.4, 0.4, 1.0);
+    }
+
     window.set_key_polling(true);
 
     while !window.should_close() {
-        window.swap_buffers();
-
         glfw.poll_events();
         for (_, event) in glfw::flush_messages(&events) {
             println!("{:?}", event);
@@ -26,5 +42,11 @@ fn main() {
                 _ => {}
             }
         }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.swap_buffers();
     }
 }
