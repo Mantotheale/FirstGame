5005fb8f2662cfe5c7bcd75253d524bc719741bd
mantotheale <alessiomantonico@gmail.com>
2026-06-01 01:10:51 +0900

    First window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate glfw;
+
+use glfw::{Action, Context, Key};
+
 fn main() {
-    println!("Hello, world!");
+    use glfw::fail_on_errors;
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+
+    let (mut window, events) = glfw
+        .create_window(300, 300, "Hello this is window", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    window.make_current();
+    window.set_key_polling(true);
+
+    while !window.should_close() {
+        window.swap_buffers();
+
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            println!("{:?}", event);
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true)
+                }
+                _ => {}
+            }
+        }
+    }
 }
